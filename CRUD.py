a81039e82a69d74f14d70d4d2162fb249d309503
hubsch <hubsch@MacBook-Pro-hubsch.local>
2025-02-28 12:48:52 +0100

    creating app with new idea to train

--- CRUD.py
+++ CRUD.py
@@ -1,46 +1,14 @@
-import datetime
-
 taskArray = []
 valid_statuses = ["ToDo", "In Progress", "Done"]
 
-def createTask(name: str, status: str, taskDueDate: str):
-    if status not in valid_statuses:
-        raise ValueError("Invalid status! Please enter 'ToDo', 'In Progress' or 'Done'.")
-    
-    try:
-        taskDueDate = datetime.datetime.strptime(taskDueDate, "%d.%m.%Y").date()
-        if taskDueDate < datetime.date.today():
-            raise ValueError("Due date cannot be in the past!")
-    except ValueError:
-        raise ValueError("Invalid date format! Use DD.MM.YYYY.")
-    
-    taskInputDate = datetime.date.today()
-    
-    task = {
-        'name': name,
-        'status': status,
-        'taskInputDate': taskInputDate.strftime('%d.%m.%Y'),
-        'taskDueDate': taskDueDate.strftime('%d.%m.%Y')
-    }
-    
-    taskArray.append(task)
-    
-    with open('./ToDo.txt', 'a') as file:
-        file.write(f"\n- {task['name']} [{task['status']}] Created: {task['taskInputDate']} | Due: {task['taskDueDate']}")
-    
-    return task
 
-# def deleteTask(task_name: str):
-#     global taskArray
-#     taskArray = [task for task in taskArray if task['name'] != task_name]
-    
-#     with open('./ToDo.txt', 'r') as file:
-#         lines = file.readlines()
-    
-#     with open('./ToDo.txt', 'w') as file:
-#         for line in lines:
-#             if not line.strip().startswith(f"- {task_name} "):
-#                 file.write(line)
-
-def deleteTask(name):
-    
+def createTask():
+    while True:
+        name = input('Type task name or quit: ')
+        if name.lower() == 'quit':
+            break
+        while True:
+            status = input('Set task status (ToDo, In Progress, Done): ')
+            # taskArray.append(name)
+            # return print(taskArray)
+            break
--- app.py
+++ app.py
@@ -1,33 +1,13 @@
-import datetime
 from CRUD import createTask
 
 taskArray = []
 
+
 while True:
-    name = input("Add new task (or type 'quit' to exit): ")
-    if name.lower() == 'quit':
+    action = input('What do you want to do? (create task, read tasks, delete task, update task or quit)')
+    if action.lower() == 'quit':
         break
-
     while True:
-        status = input("Set task status (ToDo, In Progress, Done): ")
-        if status in ["ToDo", "In Progress", "Done"]:
-            break
-        print("Invalid status! Please enter 'ToDo', 'In Progress' or 'Done'.")
-
-    while True:
-        taskDueDate = input("Enter due date (DD.MM.YYYY): ")
-        try:
-            datetime.datetime.strptime(taskDueDate, "%d.%m.%Y").date()
-            break
-        except ValueError:
-            print("Invalid date format! Use DD.MM.YYYY.")
-
-    task = createTask(name, status, taskDueDate)
-    taskArray.append(task)
-
-with open('./ToDo.txt', 'r') as readFile:
-    print(f"\nYour tasks:{readFile.read()}")
-
-print('\nNew task added:')
-for task in taskArray:
-    print(f"- {task['name']} [{task['status']}] Created: {task['taskInputDate']} | Due: {task['taskDueDate']}")
+        if action == 'create task':
+            createTask()
+    # elif action == 'read tasks':
